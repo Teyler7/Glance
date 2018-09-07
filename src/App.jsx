2d8ca3c8b29454dee6c80aed6723cc3e55630d6b
Teyler Halama <teyler.halama@gmail.com>
2018-09-07 15:54:00 -0600

    Added .babelrc, scss, and successful compilation files

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './scss/base.scss'
+
+export default class App extends React.Component {
+    render(){
+        return <div>Hello World</div>
+    }
+}
