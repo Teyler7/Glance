4c66cd715f0e2a125b990168c8cbf9ec14df02d5
Teyler <teyler.halama@gmail.com>
2019-07-07 11:12:59 -0600

    Replaced src with client

--- README.md
+++
@@ -1,2 +0,0 @@
-# Glance
-A homework management application. Provides an overview of assignments based on subject and suggests what to work on next based on filters
--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './scss/base.scss'
-
-export default class App extends React.Component {
-    render(){
-        return <div>Hello World</div>
-    }
-}
